a8f6c46dd72822e3aefdd77db0ee6800b47d552f
Farrell Arrafi Idris <jshzhbsj@gmail.com>
2024-08-22 13:41:38 +0700

    day 2 part 2

--- src/components/Todolist.tsx
+++ src/components/Todolist.tsx
@@ -34,6 +34,14 @@ export default function Todolist() {
         <li
         key={todo.id}
         className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b-2 border-slate-200"
+        onClick={() => {
+          setTodos(todos.map(t => {
+            if (t.id === todo.id) {
+              return {...t, isComplete:!t.isComplete };
+            }
+            return t;
+          }))
+        }}
         >
           <span className={`${todo.isComplete ? 'line-through text-[#ccc]':""}`}> {todo.text}</span>{""} 
           
